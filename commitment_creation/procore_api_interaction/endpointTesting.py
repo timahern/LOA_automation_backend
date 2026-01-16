68dccdb41e1c317e432cc51b1078b268aa791244
Ubuntu <ubuntu@ip-172-31-47-254.us-east-2.compute.internal>
2026-01-16 19:15:01 +0000

    minor syntax error change

--- commitment_creation/procore_api_interaction/endpointTesting.py
+++ commitment_creation/procore_api_interaction/endpointTesting.py
@@ -37,7 +37,7 @@ def makeRequest(sub_info):
     #vendor is determined through getSubNameMatch function. 
     print(f"subname is {sub_info['vendor_selected']}")
     vendor_obj = getSubNameMatch(company_id, project_id, sub_info["vendor_selected"])
-    print(f"MATCHED subName is {vendor_obj})
+    print(f"MATCHED subName is {vendor_obj}")
     if(vendor_obj):
         vendor_id = vendor_obj['id']
         bill_recipients_and_accessors = getVendorUsers(company_id, project_id, vendor_id)
